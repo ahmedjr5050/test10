87d7506d4c48b42c071b6180ab10e7957b776326
ahmedjr5050 <ahmedsabry7333@gmail.com>
2024-09-23 13:50:02 +0300

    Initial commit

---
+++ lib/t.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AA extends StatelessWidget {
+  const AA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text("احمد صبرى"),
+          ),
+          Center(
+            child: Text("AA"),
+          ),
+          Center(
+            child: Text("AA"),
+          ),
+          Center(
+            child: Text("AA"),
+          ),
+        ],
+      ),
+    );
+  }
+}
